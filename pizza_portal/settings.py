43ede9d864e8ff5f5bd4c085ef5774f19697c912
Ubuntu <ubuntu@ip-172-31-13-182.us-east-2.compute.internal>
2025-01-14 06:05:33 +0000

    Deploy Pizza Portal to domain name.

--- pizza_portal/settings.py
+++ pizza_portal/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ SECRET_KEY = config("SECRET_KEY")
 
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["onteia.dev"]
 
 
 # Application definition
@@ -114,7 +115,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_ROOT = ""
+STATIC_ROOT = "/var/www/pizza_portal/static"
 STATIC_URL = "static/"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
